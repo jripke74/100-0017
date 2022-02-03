327db48d61ab0972e41814481efb682846d8dc79
Jeff Ripke <jripke74@gmail.com>
2022-02-03 09:32:39 -0800

    add app.js, config.js and game.js

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+const editPlayer1BtnElement = document.qetEllementById('edit-player-1-btn');
+const editPlayer2BtnElement = document.qetEllementById('edit-player-2-btn');
+
+
+
+editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
+editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
