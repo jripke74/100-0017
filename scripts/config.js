c10f33b12cd1a9f3cdd18e6f10059dd635920bba
Jeff Ripke <jripke74@gmail.com>
2022-02-03 10:53:42 -0800

    add check enteredPlayername in savePlayerConfig() function

--- scripts/config.js
+++ scripts/config.js
@@ -6,11 +6,18 @@ function openPlayerConfig() {
 function closePlayerConfig() {
   playerConfigOverlayElement.style.display = 'none';
   backdropElement.style.display = 'none';
+  formElement.firstElementChild.classList.remove('error');
+  errorsOutputElement.textContent = '';
 }
 
 function savePlayerConfig(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
-  const enteredPlayername = formData.get('playername');
-  console.log(enteredPlayername);
+  const enteredPlayername = formData.get('playername').trim();
+  
+  if (!enteredPlayername) {
+    event.target.firstElementChild.classList.add('error');
+    errorsOutputElement.textContent = 'Please enter a valid name!';
+    return;
+  }
 }
