8e058762548670f859348a0dd0502e9c5f26abf7
Jeff Ripke <jripke74@gmail.com>
2022-02-03 16:26:16 -0800

    add logic to check for winner or a draw

--- scripts/game.js
+++ scripts/game.js
@@ -8,7 +8,7 @@ function startNewGame() {
   gameAreaElement.style.display = 'block';
 }
 
-function  switchPlayer() {
+function switchPlayer() {
   if (activePlayer === 0) {
     activePlayer = 1;
   } else {
@@ -32,7 +32,58 @@ function selectGameField(event) {
   event.target.classList.add('disabled');
 
   gameData[selectedRow][selectedColumn] = activePlayer + 1;
-  console.log(gameData);
+  
+  const winnerId = checkForGameOver();
+  console.log(winnerId);
 
+  currentRound++;
   switchPlayer();
+}
+
+function checkForGameOver() {
+  // Checking the rows for equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
+
+  // Checking the rows columns equality
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[0][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
+
+  // Diagonal: Top left to bottom right
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+
+  // Diagonal: Bottom left to top right
+  if (
+    gameData[2][0] > 0 &&
+    gameData[2][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[0][2]
+  ) {
+    return gameData[2][0];
+  }
+
+  if (currentRound === 9) {
+    return -1;
+  }
+
+  return 0;
 }
